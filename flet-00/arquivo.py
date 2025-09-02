cfb8dd856ebb8fbc0e1b3672ca780a67a10c36e5
Pe-drou <phsr300408@gmail.com>
2025-09-02 16:16:42 -0300

    Comentarios
    Comentários de funcionamento

--- flet-00/arquivo.py
+++ flet-00/arquivo.py
@@ -1,9 +1,15 @@
+# Habilita o uso do flet no 
 import flet as ft
 
+# Abre a página "main" 
 def main(page: ft.Page):
+    # Variável para um Input de texto
     txt_name = ft.TextField(label="Seu Nome")
+    # Vetor de botão
     def btn_click(e):
+        # Função de adicionar uma mensagem quando o botão for pressionado
         page.add(ft.Text(f'Olá, {txt_name.value}!'))
+    # Função para adiconar os elementos à pagina
     page.add(txt_name, ft.ElevatedButton("Dizer olá", on_click=btn_click))
-
+# Função para visualizar o site
 ft.app(target=main)
